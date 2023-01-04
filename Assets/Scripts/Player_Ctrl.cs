4855aa4a965493c13eb976e84bf636ede13ebeff
Polao63 <wndbsrider7@naver.com>
2023-01-04 13:51:44 +0900

    1월 4일 오전 개발중

--- Assets/Scripts/Player_Ctrl.cs
+++ Assets/Scripts/Player_Ctrl.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class Player_Ctrl : MonoBehaviour
 {
+    float h = 0f;
+    float v = 0f;
+
+    float moveSpeed = 7f;
+    Vector3 moveDir = Vector3.zero;
+
+    public GameObject m_BulletPrefab = null;
+    public GameObject m_ShootPos = null;
+    float m_ShootCool = 0f; //주기 계산
+
+    //---- 주인공 화면 밖으로 이탈하지 않도록 하기 위한 변수
+    Vector3 HalfSize = Vector3.zero;
+    Vector3 m_CacCurPos = Vector3.zero;
+
+    float a_LmtBdLeft = 0;
+    float a_LmtBdTop = 0;
+    float a_LmtBdRight = 0;
+    float a_LmtBdBottom = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +32,42 @@ public class Player_Ctrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        if (h != 0f || v != 0f)
+        {
+            moveDir = new Vector3(h, v, 0);
+
+            if (1 < moveDir.magnitude)
+            { moveDir.Normalize(); }
+
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+            // 또는 transform.Translate(moveDir * moveSpeed * Time.deltaTime);
+        }
+
+        LimitMove();
+    }
+
+    void LimitMove()
+    {
+        m_CacCurPos = transform.position;
+
+        if (m_CacCurPos.x < CameraResolution.m_ScreenWMin.x + HalfSize.x)
+        { m_CacCurPos.x = CameraResolution.m_ScreenWMin.x + HalfSize.x; }
+
+
+        if (m_CacCurPos.x > CameraResolution.m_ScreenWMax.x - HalfSize.x)
+        { m_CacCurPos.x = CameraResolution.m_ScreenWMax.x - HalfSize.x; }
+
+        if (m_CacCurPos.y < CameraResolution.m_ScreenWMin.y + HalfSize.y)
+        { m_CacCurPos.y = CameraResolution.m_ScreenWMin.y + HalfSize.y; }
+
+
+        if (m_CacCurPos.y > CameraResolution.m_ScreenWMax.y - HalfSize.y)
+        { m_CacCurPos.y = CameraResolution.m_ScreenWMax.y - HalfSize.y; }
+
+        transform.position = m_CacCurPos;
+
     }
 }
